2910ccf80dc392de4011cb46234622a6473e0833
Ramis Wachtler <ramis.wachtler@outlook.com>
2014-12-01 15:01:46 +0100

    Added some more resources
    + added Bootstrap
    + included Bootstrap to the view
    + added a stylesheet file

--- view/index.php
+++ view/index.php
@@ -3,6 +3,24 @@
  * Created by PhpStorm.
  * User: R.Wachtler
  * Date: 01.12.14
- * Time: 13:58
- */
+ */?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="/public/css/normalize-css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="/public/css/bootstrap/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="/public/css/main.css">
+    <script src="/public/components/bootstrap/bootstrap.js"></script>
+</head>
+
+    <body>
+    <div class="header row">
+        <h1>Pin Me Up</h1>
+    </div>
+    <footer>
+
+    </footer>
+    </body><!--./body-->
+</html>
+
 
