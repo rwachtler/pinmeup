901370ad2f4181fd4a31cf1cce917a333222f07f
Ramis Wachtler <ramis.wachtler@outlook.com>
2014-12-01 15:52:10 +0100

    Setted up basic geo-locating functionality
    - removed the css map requirement from boostrap.css
    + added some content to the view
    ~ styled the view

--- view/index.php
+++ view/index.php
@@ -7,20 +7,80 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="/public/css/normalize-css/normalize.css">
-    <link rel="stylesheet" type="text/css" href="/public/css/bootstrap/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="/public/css/main.css">
-    <script src="/public/components/bootstrap/bootstrap.js"></script>
+    <link rel="stylesheet" type="text/css" href="../public/css/normalize-css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="../public/css/bootstrap/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../public/css/main.css">
+    <script src="../public/components/jquery/jquery.js"></script>
+    <script src="../public/components/bootstrap/bootstrap.js"></script>
 </head>
 
-    <body>
+    <body onload="geoLocate()">
     <div class="header row">
-        <h1>Pin Me Up</h1>
+        <div class="container col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <h1>Pin Me Up!</h1>
+        </div>
     </div>
-    <footer>
+    <div class="content row">
+        <div class="container">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <h1>Facts</h1>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <h1>Map</h1>
+                <div class="row">
+                    <div id="map" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="container">
+        <p class="pull-right">&copy; 2014 Ramis Wachtler | Michael Stifer | Mario Kurzweil</p>
     </footer>
     </body><!--./body-->
+    <script>
+        var mapElement = document.getElementById("map");
+        /*
+            Checks for ability of geolocation feature
+                true - call showPosition function
+                error - call showError function
+            not supported - notify user
+        */
+        function geoLocate(){
+            if(navigator.geolocation){
+                navigator.geolocation.getCurrentPosition(showPosition, showError);
+            }
+            else{
+                mapElement.innerHTML = "Awww snap! Your browser does not support the geolocation feature!";
+            }
+        }
+        /*
+            Displays the user position
+         */
+        function showPosition(position){
+            mapElement.innerHTML = "Latituted: " + position.coords.latitude +
+            "<br> Longitude: "+position.coords.longitude;
+        }
+
+        function showError(error){
+            switch(error.code){
+                case error.PERMISSION_DENIED:
+                    mapElement.innerHTML = "Denied the request for Geolocation.";
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    mapElement.innerHTML = "Location information is not available.";
+                    break;
+                case error.TIMEOUT:
+                    mapElement.innerHTML = "The request timed out.";
+                    break;
+                case error.UNKNOWN_ERR:
+                    mapElement.innerHTML = "An unknown error occurred.";
+                    break;
+            }
+        }
+
+    </script>
 </html>
 
 
